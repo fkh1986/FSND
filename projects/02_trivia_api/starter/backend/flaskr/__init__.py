af1e96eff1d72dbfdfec3927e79a1d0233497baa
Fadi Haddad <fadi.k.haddad@gmail.com>
2020-08-12 08:32:55 +0300

    Add setup for CORS in Trivia app

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -16,10 +16,14 @@ def create_app(test_config=None):
   '''
   @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
   '''
+  cors = CORS(app, resources={r"*": {"origins":"*"}})
 
   '''
   @TODO: Use the after_request decorator to set Access-Control-Allow
   '''
+  def after_request(response):
+    response.headers.set('Access-Control-Allow-Origin', '*')
+    return response
 
   '''
   @TODO: 
