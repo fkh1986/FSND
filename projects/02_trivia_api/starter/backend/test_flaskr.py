cb3fb0c72be5f0d517fe81155ff6432e7ff97fd6
Fadi Haddad <fadi.k.haddad@gmail.com>
2020-08-13 08:42:59 +0300

    Make test for GET category's questions endpoint

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -24,7 +24,7 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         pass
@@ -34,6 +34,28 @@ class TriviaTestCase(unittest.TestCase):
     Write at least one test for each test for successful operation and for expected errors.
     """
 
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['categories'])
+        self.assertTrue(len(data['categories']))
+
+    def test_get_category_questions(self):
+        res = self.client().get('/categories/1/questions')
+        data = json.loads(res.data)
+
+        questions = Question.query.filter_by(category=1).all()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['questions'])
+        self.assertTrue(len(data['questions']) <= 10)
+        self.assertTrue(data['total_questions'])
+        self.assertEqual(data['total_questions'], len(questions))
+        self.assertEqual(data['current_category'], 1)
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
