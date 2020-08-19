00d752e59fe2bf402210c1f17cef5857b8b54a29
Fadi Haddad <fadi.k.haddad@gmail.com>
2020-08-19 23:42:35 +0300

    Add documenation to Trivia app code

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -17,6 +17,8 @@ def create_app(test_config=None):
     '''
     @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
     '''
+
+    # Setup CORS : Allow all origins for all routes
     cors = CORS(app, resources={r"*": {"origins":"*"}})
 
     '''
@@ -32,10 +34,11 @@ def create_app(test_config=None):
         This is a helper function to paginate any collection returning a single page of items
 
         The function expects:
+        request: the HTTP request to extract the page argument or use a default of 1
         collection (list): a list of the items to be paginated
-        page (int): an integer indicating the number of the page requested (returns first page if not provided)
-        count (int): number of items to be returned (currently set to a default of 10)
+        count (int): number of items to be returned per page (currently set to a default of 10)
         '''
+
         page = request.args.get('page', default=1, type=int)
         start = (page-1) * count
         end = start + count
@@ -48,12 +51,14 @@ def create_app(test_config=None):
     '''
     @app.route('/categories')
     def get_categories():
+        '''
+        Retreives all available categories
+        '''
 
         categories = Category.query.all()
 
         if not categories:
             abort(404)
-
 
         paginated_categories = paginate(request, categories)
 
@@ -80,6 +85,9 @@ def create_app(test_config=None):
     '''
     @app.route('/questions')
     def get_questions():
+        '''
+        Retrieves all available questions
+        '''
 
         questions = Question.query.all()
         categories = Category.query.all()
@@ -111,6 +119,9 @@ def create_app(test_config=None):
 
     @app.route('/questions/<int:question_id>', methods=['DELETE'])
     def delete_question(question_id):
+        '''
+        Deletes a question by its ID
+        '''
 
         question = Question.query.get(question_id)
 
@@ -141,6 +152,9 @@ def create_app(test_config=None):
     '''
     @app.route('/questions', methods=['POST'])
     def add_question():
+        '''
+        Adds a question, its answer, category and difficulty
+        '''
 
         question_data = request.json
         question = request.json['question']
@@ -172,6 +186,9 @@ def create_app(test_config=None):
     '''
     @app.route('/search', methods=['POST'])
     def search_questions():
+        '''
+        Retrieves all questions that containts the search term
+        '''
 
         data = request.json
 
@@ -179,6 +196,7 @@ def create_app(test_config=None):
             search_term = data['searchTerm']
 
         except KeyError:
+            # Raise a Bad Request if searchTerm is not in request body
             abort(400)
 
         try:
@@ -205,6 +223,9 @@ def create_app(test_config=None):
     '''
     @app.route('/categories/<int:category_id>/questions')
     def get_questions_by_category(category_id):
+        '''
+        Retrieves questions based on the selected category
+        '''
 
         questions = Question.query.filter_by(category=category_id).all()
 
@@ -237,9 +258,13 @@ def create_app(test_config=None):
     '''
     @app.route('/quizzes', methods=['POST'])
     def play_quiz():
+        '''
+        Starts a quiz
+        '''
 
         data = request.json
 
+        # Raise Bad Request if request body is missing the required data
         if not all(key in data for key in ['quiz_category', 'previous_questions']):
             abort(400)
 
@@ -247,11 +272,14 @@ def create_app(test_config=None):
         quiz_category = data['quiz_category']
 
         try:
+            # Filter out all previously used questions
             possible_questions = Question.query.filter(Question.id.notin_(previous_questions))
 
+            # Filter only questions from the selected category if not ALL categories is selected
             if quiz_category['id'] != 0:
                 possible_questions = possible_questions.filter(Question.category == quiz_category['id'])
 
+            # Randomize the returned question to allow for variation in quizzes
             next_question = possible_questions.order_by(func.random()).first()
 
             next_question = next_question.format() if next_question else None
@@ -270,6 +298,9 @@ def create_app(test_config=None):
     Create error handlers for all expected errors
     including 404 and 422.
     '''
+
+    # Error handlers for HTTP error responses
+
     @app.errorhandler(400)
     def bad_request(error):
         return jsonify({
